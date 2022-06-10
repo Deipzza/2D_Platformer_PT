8b6db9ea1200e3444939df7a7dd05d7c3b627955
David Pérez <dperezz@unal.edu.co>
2022-06-09 22:03:12 -0500

    Final commit and WebGL build

--- Assets/Scripts/DPZ/SceneController.cs
+++ Assets/Scripts/DPZ/SceneController.cs
@@ -6,6 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour {
 
+    // Botón de inicio del juego
+    public void StartGame() {
+        SceneManager.LoadSceneAsync("SampleScene");
+    }
+    
     // Botón de cerrado del canvas
     public void BackScene() {
         SceneManager.UnloadSceneAsync("Scene2"); // Descarga la escena
--- Assets/Scripts/Mechanics/VictoryZone.cs
+++ Assets/Scripts/Mechanics/VictoryZone.cs
@@ -1,6 +1,8 @@
 using Platformer.Gameplay;
 using UnityEngine;
 using static Platformer.Core.Simulation;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 namespace Platformer.Mechanics
 {
@@ -16,7 +18,20 @@ namespace Platformer.Mechanics
             {
                 var ev = Schedule<PlayerEnteredVictoryZone>();
                 ev.victoryZone = this;
+
+                // Ejecución de una co-rutina para que en un tiempo determinado ejecute una función
+                // En este caso, después de 2 segundos para ver la animación de victoria
+                StartCoroutine(ExecuteAfterTime(2f));
             }
+        }
+
+        // Enumerador para cargar asíncrona y aditivamente la escena final
+        IEnumerator ExecuteAfterTime(float time) {
+            yield return new WaitForSeconds(time); // Esperar el tiempo indicado
+
+            // Parar el juego y cargar la escena
+            Time.timeScale = 0;
+            SceneManager.LoadSceneAsync("EndScene", LoadSceneMode.Additive);
         }
     }
 }
